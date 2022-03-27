b03291b7aa2eaf3021a1a2eb575433ce8b165e22
DESKTOP-MDKATNF\gregg <gregg190@gmail.com>
2022-03-27 12:46:56 +0300

    refactoring of variable functions

--- Authorization Personal Account/test_verified_phone.py
+++ Authorization Personal Account/test_verified_phone.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import pytest
 import time
 
@@ -22,12 +23,12 @@ class TestMainPage():
 
     def test_auth_verified_phone(self, browser):
         browser.get(link)
-        browser.find_element_by_css_selector("#header > section > div > div:nth-child(3) > a").click()
-        browser.find_element_by_id("username").send_keys(login)
-        browser.find_element_by_id("password").send_keys(password)
-        browser.find_element_by_id("submit").click()
+        browser.find_element(By.CSS_SELECTOR, "#header > section > div > div:nth-child(3) > a").click()
+        browser.find_element(By.ID, "username").send_keys(login)
+        browser.find_element(By.ID, "password").send_keys(password)
+        browser.find_element(By.ID, "submit").click()
         time.sleep(1)
-        username = browser.find_element_by_css_selector("#header > section > div > div:nth-child(2) > div > a")
+        username = browser.find_element(By.CSS_SELECTOR, "#header > section > div > div:nth-child(2) > div > a")
         act_login = username.text
         if act_login == exp_login:
             print("\nAuthorization success")
@@ -36,10 +37,10 @@ class TestMainPage():
 
     def test_empty_form(self, browser):
         browser.get(link)
-        browser.find_element_by_css_selector("#header > section > div > div:nth-child(3) > a").click()
-        browser.find_element_by_id("submit").click()
-        error_notification = browser.find_element_by_css_selector("#auth-region > div > div.modal__body.l-form > "
-                                                                  "div.l-form__row.-js-error-summary.error.summary")
+        browser.find_element(By.CSS_SELECTOR, "#header > section > div > div:nth-child(3) > a").click()
+        browser.find_element(By.ID, "submit").click()
+        error_notification = browser.find_element(By.CSS_SELECTOR, "#auth-region > div > div.modal__body.l-form > "
+                                                                   "div.l-form__row.-js-error-summary.error.summary")
         act_error_empty = error_notification.text
         if act_error_empty == exp_error_empty:
             print("\nCorrect notification")
